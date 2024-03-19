b26f5d42390bf6e10ae56b9a175df248613c004d
sbi-borecki <willian.borecki@solutionsinbi.com>
2024-03-19 10:59:58 -0300

    add comentarios ini

--- inst/_init_.R
+++ inst/_init_.R
@@ -1,5 +1,7 @@
 
 
+
+# informa lista de urls para baixar, pode vir de uma planilha em excel
 lista <- c("https://amzn.to/4a733OZ",
            "https://www.amazon.com.br/dp/B0BTW3LPCF/?_encoding=UTF8&ref_=dlx_gate_dd_dcl_tlt_ffa84d84_dt_pd_gw_unk&pd_rd_w=IQ8ON&content-id=amzn1.sym.1998b660-aebe-49f8-9960-47452c1e2086&pf_rd_p=1998b660-aebe-49f8-9960-47452c1e2086&pf_rd_r=8E8BWVZV4563PRQ2KCTG&pd_rd_wg=ZQWNR&pd_rd_r=960c6405-5ed2-4a6e-98f4-c317b15bf57f&th=1",
            "https://www.magazinevoce.com.br/magazinepromobuzy/lavadora-de-roupas-consul-12kg-16-programas-de-lavagem-branca-cwh12/p/236161000/ed/lava/",
@@ -7,17 +9,22 @@ lista <- c("https://amzn.to/4a733OZ",
 
 lista <- "https://www.amazon.com.br/LYOR-Bandeja-Bambu-Marrom-Natural/dp/B08X1B72TP?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3O2G3I0BSD19&keywords=bandeja&qid=1695737914&refinements=p_72%3A17833786011&rnid=5560472011&sprefix=bande%2Caps%2C326&sr=8-5&th=1&linkCode=sl1&tag=promobuzy-20&linkId=daf4acd3815ef51f47c04843b34c479c&language=pt_BR&ref_=as_li_ss_tl"
 
+# informa diretorio onde dados serão salvos
 diretorio <- "data-raw"
 
+## Baixa Paginas da Web
 baixar_paginas(lista_url = lista, diretorio = diretorio)
 
+# Lista os arquivos baixados
 arquivos <- list.files(diretorio, full.names = T)
 
-
+# Lê arquivos, para cada loja existe um função especifica para ler dados da pagina
 dados_amz <- ler_amazon(diretorio = diretorio)
 dados_mgl <- ler_magazine_luiza(diretorio = diretorio)
 dados_mcl <- ler_mercado_livre(diretorio = diretorio)
 
+# Unifica data frames
 dados <- rbind(dados_amz,dados_mgl,dados_mcl)
 
+# Salva dados em formato xlsx na pata raiz, pode informar o diretorio completo "{seu/caminho/personalizado}/dados.xlsx"
 openxlsx::write.xlsx(dados, "dados.xlsx")
