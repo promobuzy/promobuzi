d0fd809ce7303f66b8ab517cb0f7fd185640e547
sbi-borecki <willian.borecki@solutionsinbi.com>
2024-03-28 14:26:19 -0300

    texto opicional magazine luiza

--- R/ler_magazine_luiza.R
+++ R/ler_magazine_luiza.R
@@ -21,7 +21,8 @@ ler_magazine_luiza <- function(arquivos = NULL, diretorio = ".") {
     titulo <- xml2::xml_find_first(x, ".//h1") |>
       xml2::xml_text()
 
-    texto_opcional <- NA
+    texto_opcional <- xml2::xml_find_first(x, ".//div[@data-testid= 'wrapper-badge']//img") |>
+      xml2::xml_attr("src")
 
     preco_novo <- xml2::xml_find_first(x, "//p[@data-testid= 'price-value'] ") |>
       xml2::xml_text()
