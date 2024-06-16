ac856e7e951ebd4bafc58103cb7a652f9927d65a
sbi-borecki <willian.borecki@solutionsinbi.com>
2024-06-16 20:45:43 -0300

    concorrentes

---
+++ Funcoes_R/detalhes_pechinchou.R
@@ -0,0 +1,43 @@
+detalhes_pechinchou <- function(lista_url){
+
+  h <-  c(
+    `User-Agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0")
+
+
+  pb <- progress::progress_bar$new(
+    format = " Processing [:bar] :percent in :elapsed | ETA: :eta",
+    total = length(lista_url),
+    clear = FALSE,
+    width = 60
+  )
+
+
+  purrr::map_dfr(lista_url, purrr::possibly(~{
+
+    pb$tick()
+
+    dados <- url |>
+      httr2::request() |>
+      httr2::req_perform() |>
+      httr2::resp_body_html()
+
+    cupom <- dados |>
+      xml2::xml_find_first(".//p[@class='TextCoupon']") |>
+      xml2::xml_text()
+
+    link_afiliado <- dados |>
+      xml2::xml_find_first(".//script[@id='__NEXT_DATA__']") |>
+      xml2::xml_text() |>
+      jsonlite::fromJSON() |>
+      purrr::pluck("props", "pageProps", "promo", "short_url")
+
+    produto <- dados |>
+      xml2::xml_find_first(".//script[@id='__NEXT_DATA__']") |>
+      xml2::xml_text() |>
+      jsonlite::fromJSON() |>
+      purrr::pluck("props", "pageProps", "promo", "title")
+
+    tibble::tibble(link_whatsapp = .x, cupom, produto, link_afiliado)
+
+  }, NULL))
+}
