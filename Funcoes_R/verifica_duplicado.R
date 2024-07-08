203a164fc17731dd5501e071e522b860a04bc28b
sbi-borecki <willian.borecki@solutionsinbi.com>
2024-07-08 13:41:37 -0300

    Banco Total

--- Funcoes_R/verifica_duplicado.R
+++ Funcoes_R/verifica_duplicado.R
@@ -1,9 +1,9 @@
 verifica_duplicado <- function(base_ofertas){
 
   arquivos <- list.files("~/Projetos/promobuzi/Links_IN", full.name = T) |>
-    stringr::str_subset("wc-product")
+    stringr::str_subset("Banco Total")
 
-  produtos_publicados <- purrr::map_dfr(arquivos, ~{read.csv(.x)}) |>
+  produtos_publicados <- purrr::map_dfr(arquivos, ~{openxlsx::read.xlsx(.x,sheet = 1)}) |>
     dplyr::select(Nome) |>
     dplyr::distinct() |>
     dplyr::pull()
