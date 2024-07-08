ca2a373dc474a63a3f69b6b7a6cbf331aac8fe6c
sbi-borecki <willian.borecki@solutionsinbi.com>
2024-07-08 14:15:59 -0300

    Banco Total

--- Funcoes_R/api_woocommerce.R
+++ Funcoes_R/api_woocommerce.R
@@ -9,8 +9,8 @@ make_url_query_woocomerce <- function(base_url, consumer_key, consumer_secret){
 produtos <- "https://promobuzy.com.br/wp-json/wc/v3/products"
 categories <- "https://promobuzy.com.br/wp-json/wc/v3/products/categories"
 
-consumer_key  <- "ck_53bf61fbdab289dc68c156fca321bfbd375b18b3"
-consumer_secret <-  "cs_a150b583aa697ea47226d0273339a4884a612d7a"
+consumer_key  <- ""
+consumer_secret <-  ""
 
 
 end_point <- make_url_query_woocomerce(produtos, consumer_key, consumer_secret)
